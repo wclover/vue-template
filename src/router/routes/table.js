75c0af5c86373bd87997f25d6f9ad6b9175bc716
liangJiaHao <liangjiahao@ftl-express.cn>
2021-01-07 14:38:40 +0800

    feat: add mergeTable

--- src/router/routes/table.js
+++ src/router/routes/table.js
@@ -9,8 +9,14 @@ export default {
     {
       path: 'basisTable',
       name: 'BasisTable',
-      component: () => import('@/views/Table'),
+      component: () => import('@/views/Table/BasicTable'),
       meta: { name: '基础表格', icon: 'el-icon-star-off' }
+    },
+    {
+      path: 'mergeTable',
+      name: 'MergeTable',
+      component: () => import('@/views/Table/MergeTable'),
+      meta: { name: '合并表格', icon: 'el-icon-star-off' }
     }
   ]
 }
